0ebb5bb5cb75f2b7a96393224f664f5a0d948859
bernardosfz <bernardofattini@gmail.com>
2025-06-13 19:00:40 -0300

    alterada lista de beacons para dicionario, chave e valor. Adapdata função de leitura dos beacons e composição da mensagem enviada a fila

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ antena = 1
 #serial_port = serial.Serial(serial_port_name, 9600)
 time.sleep(2)
 
-TARGET_UUIDS = [
-    "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6",
-    "967D8C06-7E9D-44C3-9E12-43DCC0E6C1F6"
-]
+BEACONS = {
+    "Celular Sozo":"2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6",
+    "Celular Otavio":"967D8C06-7E9D-44C3-9E12-43DCC0E6C1F6"
+}
 
 parameters = pika.URLParameters(RABBIT_CONFIG)
 connection = pika.BlockingConnection(parameters)
@@ -26,8 +26,9 @@ channel.exchange_declare(exchange='track_beacons', exchange_type='topic', durabl
 channel.queue_declare(queue='tracking')
 
 async def main():
-    targets = [uuid.lower().replace('-', '') for uuid in TARGET_UUIDS]
-    print(f"Tracking Beacons")
+    uuid_values = list(BEACONS.values())
+    targets = [uuid.lower().replace('-', '') for uuid in uuid_values]
+    print(f"Procurando por Beacons...")
 
     while True:
         beacons = await BleakScanner.discover(timeout=5.0)
@@ -40,12 +41,14 @@ async def main():
                         hex_data = data.hex()
                         for i, target in enumerate(targets):
                             if target in hex_data:
-                                uuid_detectado = TARGET_UUIDS[i]
+                                uuid_detectado = uuid_values[i]
+                                beacon_nome = next(nome for nome, uuid in BEACONS.items() if uuid == uuid_detectado)
                                 #serial_port.write(b"1\n")
                                 message = {
                                     "antena": antena,
                                     "datahora": data_hora,
-                                    "beacon": uuid_detectado,
+                                    "beacon_uuid": uuid_detectado,
+                                    "beacon_nome": beacon_nome,
                                     "rssi": b.rssi,
                                     "mac": b.address
                                 }
